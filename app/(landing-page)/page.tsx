db8eee032c21517bcef2b12932992a61609c0b6b
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2024-02-03 14:59:49 +0700

    third-section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Hero from "./hero-section";
 import SecondSection from "./second-section";
+import ThirdSection from "./third-section";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <SecondSection />
-      
+      <ThirdSection />
    </div>
   );
 }
