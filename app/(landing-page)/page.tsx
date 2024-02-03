7cb751f87ce73ca34dd95fbdb7f2006941a5aab7
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2024-02-03 14:43:45 +0700

    Second Section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Hero from "./hero-section";
+import SecondSection from "./second-section";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <SecondSection />
+      
    </div>
   );
 }
